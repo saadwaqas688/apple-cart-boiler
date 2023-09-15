379c67b4f108280d3ddfbb46ae8e0b0eac6a406b
saad waqas <saadwaqas688@gmail.com>
2023-09-15 14:41:12 +0500

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx/testing</code>
+          <code className={styles.code}>src/app/page.tsx/testing two</code>
         </p>
         <div>
           <a
