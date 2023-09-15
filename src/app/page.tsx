ed46992b6b046f22f184454410d6bf4a421ebb6f
saad waqas <saadwaqas688@gmail.com>
2023-09-15 15:18:46 +0500

    feat(): just checkot (#abc)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("errotasdfasdfasf");
   var a;
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tasdfasx</code>
-
-
-
-
-
-
-
-
-
-
-
-          
+          <code className={styles.code}>src/app/page.tasdfasxfdsga</code>
         </p>
         <div>
           <a
