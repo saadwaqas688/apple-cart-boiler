8562bb6db3eec5dd0192fd8b3b4bbad826826480
saad waqas <saadwaqas688@gmail.com>
2023-09-15 15:04:10 +0500

    feat(): just checkot (#12345)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx/testing two</code>
+          <code className={styles.code}>src/app/page.tsx/test</code>
         </p>
         <div>
           <a
