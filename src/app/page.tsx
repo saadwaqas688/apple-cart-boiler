4d8dbb6c7a29599efa2a0a8bbafa2f48c2fe2538
saad waqas <saadwaqas688@gmail.com>
2023-09-15 15:07:09 +0500

    feat(): just checkot (#12345)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("errot");
+  console.log("errotasdfasdfasf");
   var a;
   return (
     <main className={styles.main}>
